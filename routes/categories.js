5759c74413eb47924cb0427473e7be6c446c8f65
khushal.vyas <khushal.vyas@hashedin.com>
2021-04-23 17:32:48 +0530

    CRUD APIs for Categories

---
+++ routes/categories.js
@@ -0,0 +1,90 @@
+const {Category} = require('../models/category');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) =>{
+    const categoryList = await Category.find();
+
+    if(!categoryList) {
+        res.status(500).json({success: false})
+    } 
+    res.status(200).send(categoryList);
+})
+
+// Gets only id for all categories
+router.get('/:id', async(req,res)=>{
+
+    const category = await Category.findById(req.params.id);
+
+    if(!category) {
+        res.status(500).json({message: 'The category with the given ID was not found.'})
+    } 
+    res.status(200).send(category);
+})
+
+// Gets product for a specific category id
+router.get('/category/:id', async(req,res)=>{
+    
+    const category = await Category.findById(req.params.id).populate('product');
+
+    if(!category) {
+        res.status(500).json({message: 'The category with the given ID was not found.'})
+    } 
+    res.status(200).send(category);
+})
+
+router.post('/', async (req,res)=>{
+    let prods1= await Category.find().populate("products")
+    let listOfprods=[]
+    prods1.map((p)=>{
+        p.get('product').map((a)=>{
+            listOfprods.push(a)
+        })
+        console.log(listOfprods);
+        console.log("*************");
+    })   
+    listOfprods.push(req.body.prod_id)
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        image: req.body.image,
+        product: listOfprods,
+    })
+    category = await category.save();
+
+    if(!category)
+    return res.status(400).send('the category cannot be created!')
+
+    res.send(category);
+})
+
+router.put('/:id',async (req, res)=> {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon || category.icon,
+            color: req.body.color,
+        },
+        { new: true}
+    )
+
+    if(!category)
+    return res.status(400).send('the category cannot be created!')
+
+    res.send(category);
+})
+
+router.delete('/:id', (req, res)=>{
+    Category.findByIdAndRemove(req.params.id).then(category =>{
+        if(category) {
+            return res.status(200).json({success: true, message: 'the category is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "category not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
+})
+
+module.exports =router;
