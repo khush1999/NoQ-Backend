c5b6aa632b9de95f752af7af8abd7e6bb9dd013f
Sabata <psabata@deloitte.com>
2021-04-23 16:06:05 +0530

    Setup connection to MongoDB Atlas DB

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan')
+const mongoose = require("mongoose");
+
 require('dotenv').config()
 
 const api=process.env.API_URL
@@ -16,6 +18,29 @@ app.get(`${api}/`, (req,res) => {
     res.send("Hello There!");
 })
 
+//Sample Get a Product
+app.get(`${api}/products`, (req,res) => {
+    const product = {
+            id:1,
+            name:'soya bean',
+            image:'some_url'
+    }
+    res.send(product);
+})
+
+//Database
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "qlessshopping",
+  })
+  .then(() => {
+    console.log("Database Connection is ready...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const host = 'localhost';
 const port = 3000;
